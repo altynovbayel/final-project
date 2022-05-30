1390651af1306a4ade7c25ce9658868880860d9d
altynovbayel <altynovich30@gmail.com>
2022-05-30 15:30:12 +0600

    first commit

---
+++ src/pages/ShoppingCart/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ShoppingCart(props) {
+  return (
+    <div>
+      <h1>
+        ShoppingCart
+      </h1>
+    </div>
+  );
+}
+
+export default ShoppingCart;
