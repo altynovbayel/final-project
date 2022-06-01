12c1d58b526e45cad1db5e39c1e5211ebe2e2a09
altynovbayel <altynovich30@gmail.com>
2022-06-01 16:21:17 +0600

    bayel: remove category code

--- src/pages/Category/index.jsx
+++ src/pages/Category/index.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import {useParams} from "react-router-dom";
-import {ProductList} from "../../utils/List";
-import Card from "../../components/Cards/Card";
 
 const Category = () => {
 	const {category} = useParams()
-  const categoryList = ProductList.filter(item => item.category === category)
 	return (
 		<div>
-      {
-        <Card list={categoryList}/>
-      }
+      {category}
 		</div>
 	);
 };
